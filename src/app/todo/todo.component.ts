20ec1e95558e71517b6c1916e3e66d59c5d7858d
breen1em <erin.breen@icloud.com>
2020-08-18 21:31:40 -0400

    lab submission

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Todo } from '../interfaces/todo';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-todo',
@@ -21,8 +22,39 @@ export class TodoComponent implements OnInit {
       completed: true,
     },
   ];
+  searchTerm: string;
 
   constructor() {}
+
+  addTask(form: NgForm) {
+    let newTask: Todo = {
+      task: form.value.task,
+      completed: false
+    };
+    this.todos.push(newTask);
+  }
+
+  removeTask(index: number) {
+    this.todos.splice(index, 1);
+  }
+
+  completeTask(index: number) {
+    this.todos[index].completed = true;
+  }
+
+  setSearchTerm(form:NgForm) {
+    this.searchTerm = form.value.searchTerm.trim().toLowerCase();
+  }
+  filter() {
+    if (!this.searchTerm) {
+      return this.todos;
+    } else {
+      return this.todos.filter((todos) => {
+        let currentTodo = todos.task.toLowerCase().trim();
+        return currentTodo.includes(this.searchTerm);
+      });
+    }
+  }
 
   ngOnInit(): void {}
 }
